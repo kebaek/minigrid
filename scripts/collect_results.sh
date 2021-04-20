7e834022646edcc95464dc1b706c92060277b69c
kebaek <ke.baek@berkeley.edu>
2021-04-19 22:22:58 -0400

    run multiple seeds

--- scripts/collect_results.sh
+++ scripts/collect_results.sh
@@ -1,5 +1,7 @@
-for i in 200 300 400 500 600 700 800 900 1000
+for i in 100 200 300 400 500 600 700 800 900 1000
 do
-  python3 -m scripts.qlearn --env MiniGrid-FourDoor-v0 --num_episode $i
-  python3 -m scripts.qlearn_evaluate --env MiniGrid-FourDoor-v0 --num_episode $i --dir storage/MiniGrid-FourDoor-v0/aQL/lr0.10_discount0.90_eps0.80
+  for j in 10 20 30 40 50 60 70 80 90 100
+  do
+    python3 -m scripts.qlearn_evaluate --env MiniGrid-Maze-v0 --num_episode $j --seed $i --dir storage/MiniGrid-Maze-v0/aQL/lr0.10_discount0.90_eps0.80
+  done
 done
--- scripts/qlearn_evaluate.py
+++ scripts/qlearn_evaluate.py
@@ -87,9 +87,9 @@ print('reaches winning state:' + str(is_terminal))
 print('total steps: %d'%current_length)
 print('total reward: %d'%total_reward)
 file = open(model_dir + '/log.csv', "a+")
-fieldnames = ['training episodes', 'completed', 'reward', 'steps to completion']
+fieldnames = ['training episodes', 'completed', 'reward', 'steps to completion', 'seed']
 writer = csv.DictWriter(file, fieldnames=fieldnames)
-writer.writerow({'training episodes':args.num_episode, 'reward':total_reward, 'steps to completion':current_length, 'completed': is_terminal})
+writer.writerow({'training episodes':args.num_episode, 'seed': args.seed, 'reward':total_reward, 'steps to completion':current_length, 'completed': is_terminal})
 
 if args.gif:
     print("Saving gif... ", end="")
