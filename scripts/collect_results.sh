5041249165d3956598dc1d62cf0f2c56f0328a46
kebaek <ke.baek@berkeley.edu>
2021-05-25 23:14:06 -0400

    fix script

--- scripts/collect_results.sh
+++ scripts/collect_results.sh
@@ -1,13 +1,15 @@
-for i in 100 200 300 400 500 600 700 800 900 1000
+for ((i=1;j<=100;j+=1));
 do
   for ((j=1200;j<=2000;j+=200)); do
-      python3 -m scripts.qlearn --env MiniGrid-FourDoor-v0 --num_episode $j --seed $i --dir 1000
+      python3 -m scripts.qlearn --env MiniGrid-Maze-v0 --num_episode $j --seed $i
+      python3 -m scripts.qlearn --env MiniGrid-Maze-Intermediate-v0 --num_episode $j --seed $i
   done
 done
 
-for i in 100 200 300 400 500 600 700 800 900 1000
+for ((i=1;j<=100;j+=1));
 do
   for ((j=1200;j<=2000;j+=200)); do
-      python3 -m scripts.qlearn_evaluate --env MiniGrid-FourDoor-v0 --gif "gifs-1000/${j}_${i}" --num_episode $j --seed $i --dir storage/MiniGrid-FourDoor-v0/1000/aQL/lr0.10_discount0.90_eps0.80
+      python3 -m scripts.qlearn_evaluate --env MiniGrid-Maze-v0 --num_episode $j --seed $i --dir storage/MiniGrid-Maze-v0/aQL/lr0.10_discount0.90_eps0.80
+      python3 -m scripts.qlearn_evaluate --env MiniGrid-Maze-Intermediate-v0 --num_episode $j --seed $i --dir storage/MiniGrid-Maze-Intermediate-v0/aQL/lr0.10_discount0.90_eps0.80
   done
 done
