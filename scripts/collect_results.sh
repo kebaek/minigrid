875e4c3371d9c7206df13e0f862a8405cde92425
kebaek <ke.baek@berkeley.edu>
2021-05-26 12:51:55 -0400

    a

--- scripts/collect_results.sh
+++ scripts/collect_results.sh
@@ -1,15 +1,13 @@
 for ((i=1;i<=100;i+=1));
 do
-  for ((j=3;j<=30;j+=3)); do
-      python3 scripts/qlearn.py --env MiniGrid-Maze-v0 --num_episode $j --seed $i
-      python3 scripts/qlearn.py --env MiniGrid-Maze-Intermediate-v0 --num_episode $j --seed $i
+  for ((j=20;j<=160;j+=20)); do
+      python3 scripts/qlearn.py --env MiniGrid-ThreeDoor-v0 --num_episode $j --seed $i --dir sparse
   done
 done
 
 for ((i=1;i<=100;i+=1));
 do
-  for ((j=3;j<=30;j+=3)); do
-      python3 scripts/qlearn_evaluate.py --env MiniGrid-Maze-v0 --num_episode $j --seed $i --dir storage/MiniGrid-Maze-v0/aQL/lr0.10_discount0.90_eps0.80
-      python3 scripts/qlearn_evaluate.py --env MiniGrid-Maze-Intermediate-v0 --num_episode $j --seed $i --dir storage/MiniGrid-Maze-Intermediate-v0/aQL/lr0.10_discount0.90_eps0.80
+  for ((j=20;j<=160;j+=20)); do
+      python3 scripts/qlearn_evaluate.py --env MiniGrid-ThreeDoor-v0 --num_episode $j --seed $i --dir storage/MiniGrid-ThreeDoor-v0/aQL/lr0.10_discount0.90_eps0.80
   done
 done
