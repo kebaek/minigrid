b8c03f80cf70eda3c4dda68c7200ec20a1e049c7
kebaek <ke.baek@berkeley.edu>
2021-06-01 05:54:25 -0400

    update

--- scripts/collect_results.sh
+++ scripts/collect_results.sh
@@ -1,5 +1,16 @@
 for ((i=1;i<=100;i+=1));
 do
-  for ((j=20;j<=160;j+=20)); do
-      python3 scripts/qlearn.py --env MiniGrid-ThreeDoor-v0 --num_episode $j --seed $i --dir sparse
+ for j in 3150; do
+     python3 scripts/qlearn.py --env MiniGrid-FourDoor-v0 --num_episode $j --seed $i --dir 1000
+     python3 scripts/qlearn.py --env MiniGrid-FourDoor-v0 --num_episode $j --seed $i --dir 1000
+     python3 scripts/qlearn.py --env MiniGrid-10FourDoor-v0 --num_episode $j --seed $i --dir 1000
   done
+done
+
+for ((i=1;i<=100;i+=1));
+do
+  for j in 3150; do
+      python3 scripts/qlearn_evaluate.py --env MiniGrid-FourDoor-v0 --num_episode $j  --seed $i --dir storage/MiniGrid-FourDoor-v0/1000/aQL/lr0.10_discount0.90_eps0.80
+      python3 scripts/qlearn_evaluate.py --env MiniGrid-10FourDoor-v0 --num_episode $j  --seed $i --dir storage/MiniGrid-10FourDoor-v0/1000/aQL/lr0.10_discount0.90_eps0.80
+  done
+done
